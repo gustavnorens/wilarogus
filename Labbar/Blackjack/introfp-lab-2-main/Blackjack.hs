d6e6fbcc86e5654e79c621d64a6c9a1535e63ace
gustavnorens <gustav01.noren@gmail.com>
2022-09-14 16:43:19 +0200

    Blackjack part b

--- Labbar/Blackjack/introfp-lab-2-main/Blackjack.hs
+++ Labbar/Blackjack/introfp-lab-2-main/Blackjack.hs
@@ -1,5 +1,5 @@
 {-
-Grupp
+Grupp 10
 William Flodin,
 Gustav Norén
 Aron Karlsson
@@ -12,7 +12,7 @@ import RunGame
 -- Task A1
 -- | Constructor of a hand containing 2 of Hearts and Jack of Spades
 hand2 :: Hand
-hand2 = [Card (Numeric 2) Hearts, Card  Jack Spades]
+hand2 = [Card (Numeric 2) Hearts, Card Jack Spades]
 
 -- | Våran implementation av size hand2
 {-
@@ -67,9 +67,7 @@ valueRank :: Rank -> Int
 valueRank (Numeric x) = x
 valueRank r 
   | r == Ace = 11
-  | r == King = 10
-  | r == Queen = 10
-  | r == Jack = 10
+  | otherwise = 10
 
 -- | Funktion som given ett kort returnerar värdet på kortet i form av en int
 valueCard :: Card -> Int
@@ -83,18 +81,18 @@ numberOfAces (x:xs)
   | otherwise = numberOfAces xs
 
 -- | Returnerar det sammanlagda värdet på den givna handen använder sig av funktionen
--- | valueCheck för att kolla om värdet är större än 21 och sedan kör value igen och
+-- | value' för att kolla om värdet är större än 21 och sedan kör value igen och
 -- | byter all ess som har värde 11 till värde 1 och räknar ut värdet igen
 value :: Hand -> Int
 value [] = 0
 value (x:xs) 
-  | valueCheck (x:xs) > 21 = (valueCard x + value xs) - 10 * numberOfAces (x:xs)
+  | value' (x:xs) > 21 && numberOfAces (x:xs) > 0 = value' (x:xs) - 10 * numberOfAces (x:xs)
   | otherwise = valueCard x + value xs
 
--- | Kollar det orginella värdet på value där ett ess är värt 11
-valueCheck :: Hand -> Int 
-valueCheck [] = 0 
-valueCheck (x:xs) = valueCard x + valueCheck xs
+-- | Helper funktion till Value
+value' :: Hand -> Int 
+value' [] = 0 
+value' (x:xs) = valueCard x + value' xs
 
 --Task A4
 -- | Funktion som kollar om den givna handen har bustat
@@ -106,7 +104,7 @@ gameOver h
 -- | Funktion som returnerar en vinnare av två givna händer
 winner :: Hand -> Hand -> Player
 winner g b 
-  | gameOver g == True = Bank
+  | gameOver g = Bank
   | value b >= value g && value b < 22 = Bank
   | otherwise = Guest
 
@@ -127,8 +125,19 @@ draw :: Deck -> Hand -> (Deck, Hand)
 draw [] _ = error "Deck is empty."
 draw (x:xs) y = (xs, (x:y))
 
-playBank :: Deck -> Hand
-playBank xs = undefined
+playBank :: Deck -> Hand 
+playBank xs = playBank' xs []
 
-playBank' :: Deck -> Hand -> (Deck, Hand)
-playBank' deck bankHand = draw deck bankHand
+playBank' :: Deck -> Hand -> Hand
+playBank' xs ys 
+  | value ys > 15 = ys
+  | otherwise = playBank' (fst (draw xs ys)) (snd (draw xs ys))
+
+
+doubleList = [0.182, 0.27273,]
+shuffle :: [Double] -> Deck -> Deck
+shuffle (x:xs) (d:ds)
+  | size (d:ds) 
+  | take 2 (drop 2 (show x))
+
+ 
